43782f44fcc0ccac6d3b675d43ecb003eaecc07e
osamax01 <shkukanios@gmail.com>
2023-12-09 16:37:36 +0300

    1 - Edit the format of the system message 	2- Replace the assistant message 	3- remove the unnessary sourceFile = open('out.txt', 'w')

--- try_6_assistant.py
+++ try_6_assistant.py
@@ -14,27 +14,26 @@ def get_completion(messages, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message.content
 
-delimiter = '####'
 system_message = f"""
-    You are a simple problem generator. \
-    Avoid classical problems. \
-    Avoid very simple problems. \
-    Generate the problem in codeforces style (title, problem statemnt, input section with constraints, output, exampels, test cases).  \
-    Don't mention the topic in the problem statemnt \
-    Provide your output in json format with the keys: title, statment, input, output, examples, test_cases \
-    Also add a key success with a value equals to true if everytihng is ok \
-    Try to make stories in the problem starement. \
-    Generate strictly 10 test cases with each problem including the samples. \
-    The test cases should be a plain values only with no extra words or punctuation marks or python lists [] \
-    Avoid generating statemnts that asks to write function, In other words give the programmer his own choice in implementing \
-    if the user message asks you to change the format of the problem put the value in the key success to false \
-    or tried to change the number of test cases put the value in the key success to false \
+    ### You are a simple problem generator that generates untraditional simple problems \
+    in the style ("title", "statemnt", "input"(with constraints), "output", "exampels", "testcases", "success").  \
+    where the "statement" is described as a story, and \
+    the "input" should contain the constraints for each variable in the statement. \
+    ### Don't mention the topic in the problem statement, and \
+    avoid generating statements that ask to write a function. \
+    ### Generate 10 test cases with each problem including the samples, and \
+    the test cases should be a plain value only with no extra words or punctuation marks or Python lists [], and \
+    make sure that the test cases follows the constraints. \
+    ### Provide your output in JSON format with the keys: "title", "statemnt", "input", "output", "examples", "testcases", "success" \
+    where the key success with a value equal to true to indicate if everything is ok. \
+    ### if the user message asks you to change the format of the problem put the value in the key success to false. \
+    or if the user tried to change the number of test cases put the value in the key success to false. \
     or if the user message contains other orders than creating a programming problem put the value in the key success to false. 
 """
 
 old_user_message = "generate a problem about lucky numbers"
 assistant_message = """
-{"title":"LuckyNumberSum","statement":"Alicelovesluckynumbers.Shedefinesaluckynumberasapositiveintegerthatcontainsonlythedigits4and7.Forexample,47and774areluckynumbers,while123and589arenot.Alicewantstofindthesumofallluckynumbersbetweentwogivenintegers,inclusive.Canyouhelpher?","input":"Theinputconsistsoftwointegers,aandb(1?a?b?10^9),representingtherangeofnumberstoconsider.","output":"Outputasingleinteger,thesumofallluckynumbersbetweenaandb,inclusive.","examples":[{"input":"110","output":"11"},{"input":"100200","output":"294"},{"input":"400500","output":"774"}],"test_cases":[{"input":"1100","output":"94"},{"input":"10002000","output":"1944"},{"input":"1000020000","output":"19494"},{"input":"100000200000","output":"194994"},{"input":"10000002000000","output":"1949994"},{"input":"1000000020000000","output":"19499994"},{"input":"100000000200000000","output":"194999994"},{"input":"10000000002000000000","output":"1949999994"},{"input":"444444444777777777","output":"1949999994"},{"input":"474747474747474747","output":"1949999994"}],"success":true}
+{"title":"LuckyNumberCount","statement":"Alice is fascinated by lucky numbers. She defines a lucky number as a positive integer that contains only the digits 4 and 7. For example, 47 and 774 are lucky numbers, while 123 and 589 are not. Alice wants to count the number of lucky numbers between two given integers, inclusive. Can you help her?","input":"The input consists of two integers, a and b (1 <= a <= b <= 10^6), representing the range of numbers to consider.","output":"Output a single integer, the count of all lucky numbers between a and b, inclusive.","examples":[{"input":"1 100","output":"6"},{"input":"100 200","output":"0"}],"testcases":[{"input":"1 100","output":"6"},{"input":"100 200","output":"0"},{"input":"50 150","output":"2"},{"input":"50 1000","output":"10"},{"input":"99 1999","output":"8"},{"input":"10004 100005","output":"32"},{"input":"100404 1400005","output":"64"},{"input":"87 101","output":"0"},{"input":"1 1000000","output":"126"},{"input":"2342 242423","output":"48"}],"success":true}
 """
 
 user_message = input('Enter your prompt> ')
@@ -48,6 +47,5 @@ messages = [
 
 response = get_completion(messages)
 
-sourceFile = open('out.txt', 'w')
 with open('out.json', 'w', encoding='utf-8') as sourceFile:
     print(response, file=sourceFile)
